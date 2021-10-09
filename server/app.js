4146e427a473a06729e12bb5fc9b64d0fd49439a
Abidin K <abidinkofficial@gmail.com>
2021-10-09 10:38:23 +0300

    Basic socket implemented

--- server/app.js
+++ server/app.js
@@ -6,8 +6,27 @@ const app = express()
 const httpServer = http.createServer(app)
 const io = new Server(httpServer)
 
+const data = {
+  option1: 0,
+  option2: 0,
+  option3: 0,
+  option4: 0
+}
+
 app.get("/", (req, res) => {
   res.json({"message": "Voting application server is running."})
+})
+
+io.on("connection", (socket) => {
+  console.log("A user is connected.")
+
+  socket.emit("data", data)
+
+  socket.on("data", (receivedData) => {
+    console.log(`Received data: ${receivedData}`)
+    data = receivedData
+    io.emit("data", data)
+  })
 })
 
 app.listen(5000, () => {
